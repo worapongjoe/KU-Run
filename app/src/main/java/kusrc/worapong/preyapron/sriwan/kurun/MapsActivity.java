570adbe97c818ca6aa5a472d2b314a784c97a33c
worapong <joe_shincengumi@hotmail.com>
2016-04-05 17:43:21 +0700

    location ok

--- app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/MapsActivity.java
+++ app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/MapsActivity.java
@@ -1,7 +1,14 @@
 package kusrc.worapong.preyapron.sriwan.kurun;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +20,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private boolean gpsABoolean, networkABoolean;
+    private double myLatADouble, myLngADouble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +33,147 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Setup
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+        //My Loop
+        //myLoop();
+
+    }   // Main Method
+
+    private void myLoop() {
+
+        Log.d("5April", "Location = " + myLatADouble + " , " + myLngADouble);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                myLoop();
+            }
+        }, 3000);
+
+    } // myLoop
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+        myLatADouble = 13.668066;
+        myLngADouble = 100.622454;
+
+        Location networkLocation = findLocation(LocationManager.NETWORK_PROVIDER, "Cannot Connected Internet");
+        if (networkLocation != null) {
+            myLatADouble = networkLocation.getLatitude();
+            myLngADouble = networkLocation.getLongitude();
+        }
+        Location gpsLocation = findLocation(LocationManager.GPS_PROVIDER, "No Card GPS");
+        if (gpsLocation != null) {
+            myLatADouble = gpsLocation.getLatitude();
+            myLngADouble = gpsLocation.getLongitude();
+        }
+
+    }   // onResume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public Location findLocation(String strProvider,
+                                 String strError) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider,
+                    1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Log.d("5April", strError);
+        }
+
+        return location;
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    //Class
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            myLatADouble = location.getLatitude();
+            myLngADouble = location.getLongitude();
+
+        }   // onLocationChange
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+        goToCenterMap();
+
+        createAllMarker();
+
+    }   // onMapReady
+
+    private void createAllMarker() {
+
+        mMap.clear();
+
+        //for user
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(myLatADouble, myLngADouble)));
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                createAllMarker();
+            }
+        }, 3000);
+
+    }   // createAllMarker
+
+    private void goToCenterMap() {
+
+        try {
+
+            LatLng centerLatLng = new LatLng(myLatADouble, myLngADouble);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, 16));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }   // toToCenterMap
+
+}   // Main Class
